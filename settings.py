90015b5d80d3a3fe62a370aa5d99317bd4ebbcc8
IndhukuriSravani <indhukurisravani25@gmail.com>
2025-07-22 11:19:22 +0530

    settings.py

--- settings.py
+++ settings.py
@@ -25,3 +25,7 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS += ['rest_framework', 'rest_framework.authtoken', 'drf_yasg']
+REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication']}
+
+
